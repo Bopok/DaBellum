776ccf69dd576128824efc92dafaac60ac3862fc
Bopok <drew10591@gmail.com>
2016-07-28 06:21:25 -0400

    fef

---
+++ src/com/bopok/dabellum/math/Vector3f.java
@@ -0,0 +1,7 @@
+package com.bopok.dabellum.math;
+
+/**
+ * Created by Bopok on 7/28/2016.
+ */
+public class Vector3f {
+}
---
+++ src/com/bopok/dabellum/util/FileUtils.java
@@ -0,0 +1,7 @@
+package com.bopok.dabellum.util;
+
+/**
+ * Created by Bopok on 7/28/2016.
+ */
+public class FileUtils {
+}
